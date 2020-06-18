0386f3a303a071ebdde9e289255cd54df32fb28e
uchchhwash <uchchhwash.git@gmail.com>
2020-06-18 19:30:44 +0600

    inital routes setup

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = requrie("ejs");
+const ejs = require("ejs");
 
 const app = express();
 
 app.use(express.static("public"));
-app.set("view enginer", "ejs");
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", function(req, res) {
-    console.log("");
+    res.render("home");
+})
+app.get("/login", function(req, res) {
+    res.render("logic");
+})
+app.get("/register", function(req, res) {
+    res.render("register");
 })
 
 app.listen(3000, function() {
