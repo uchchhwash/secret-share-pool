ed84c74d34314c99a21a94c617686a3e17b4eb72
uchchhwash <uchchhwash.git@gmail.com>
2020-06-18 19:28:09 +0600

    initial dependency setup

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 //jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = requrie("ejs");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view enginer", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/", function(req, res) {
+    console.log("");
+})
+
+app.listen(3000, function() {
+    console.log("server started on http://localhost:3000");
+})
