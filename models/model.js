4ab4aaf48d3cf964abe555ff50e162e0d801c799
uchchhwash <uchchhwash.git@gmail.com>
2020-06-19 04:46:17 +0600

    google sign in implement

--- models/model.js
+++ models/model.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 //const encrypt = require("mongoose-encryption");
-
+var findOrCreate = require('mongoose-findorcreate')
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    secret: String
 })
 
 //used previously for mongoose-encryption
 //userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] }); 
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 exports.userSchema = userSchema;
