adea2148d20542854ed420e644d88f8a77f81c5a
uchchhwash <uchchhwash.git@gmail.com>
2020-06-18 23:44:57 +0600

    passportjs session passportlocalmongoose implement

--- app.js
+++ app.js
@@ -4,23 +4,37 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const userSchema = require("./models/model");
+const session = require("express-session");
+const passport = require("passport");
 const schema = require(__dirname + "/models/model.js")
 //const md5 = require("md5");
-const bcrypt = require("bcrypt");
+//const bcrypt = require("bcrypt");
+//const saltRounds = 10;
 const app = express();
-const saltRounds = 10;
+
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+    secret:"test",
+    resave: false,
+    saveUninitialized: false 
+}))
+
+app.use(passport.initialize());
+app.use (passport.session());
 
 mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-
+mongoose.set("useCreateIndex", true)
 const User = new mongoose.model("User", schema.userSchema);
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get("/", function (req, res) {
     res.render("home");
@@ -35,7 +49,7 @@ app.post("/register", function (req, res) {
     bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
         const newUser = new User({
             email: req.body.username,
-            password: hash
+            password: req.body.password
         })
         newUser.save(function (err) {
             if (!err) {
@@ -52,22 +66,14 @@ app.post("/register", function (req, res) {
 app.post("/login", function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
-    User.findOne({ email: username }, function (err, foundUser) {
-        if (err) {
-            console.log(err);
-        } else {
-            if (foundUser) {
-                bcrypt.compare(password, foundUser.password, function(err, result){
-                    if(result === true){
-                        console.log("login successful");
-                        res.render("secrets");
-                    }
-                })
-            }
-        }
-    })
+  
 })
 
 app.listen(3000, function () {
     console.log("server started on http://localhost:3000");
 })
+
+//bcrypy hash function
+// bcrypt.hash(req.body.password, saltRounds, function (err, hash) {})
+//bcrypt compare function
+// bcrypt.compare(password, foundUser.password, function(err, result){})
--- models/model.js
+++ models/model.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const passportLocalMongoose = require("passport-local-mongoose");
+//const encrypt = require("mongoose-encryption");
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 })
 
-//userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+//used previously for mongoose-encryption
+//userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] }); 
 
+userSchema.plugin(passportLocalMongoose);
 exports.userSchema = userSchema;
