0733fc74b348b327e2ea18c4a7bf1fe1069fb352
uchchhwash <uchchhwash.git@gmail.com>
2020-06-19 07:42:01 +0600

    test deploy

--- app.js
+++ app.js
@@ -190,10 +190,13 @@ app.get("/logout", function (req, res) {
     res.redirect("/");
 })
 
-app.listen(3000, function () {
-    console.log("server started on http://localhost:3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+app.listen(port, function() {
+    console.log("server started on : http://localhost:3000");
 })
-
 //bcrypy hash function
 // bcrypt.hash(req.body.password, saltRounds, function (err, hash) {})
 //bcrypt compare function
